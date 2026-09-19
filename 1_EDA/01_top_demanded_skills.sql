2a8085630410b0929045cec734d74d0a6861bd57
Femi Ajayi <femiebenezerajayi@gmail.com>
2026-09-18 21:00:26 -0500

    Indent Query

--- 1_EDA/01_top_demanded_skills.sql
+++ 1_EDA/01_top_demanded_skills.sql
@@ -17,13 +17,14 @@ FROM skills_dim;
 
 
 SELECT sd.skills,
-COUNT(jfp.*) AS demand_count
+  COUNT(jfp.*) AS demand_count
 FROM job_postings_fact AS jpf
 INNER JOIN skills_job_dim AS sjd
-ON jpf.job_id = sjd.job_id
+  ON jpf.job_id = sjd.job_id
 INNER JOIN skills_dim sd
-ON sjd.skill_id = sd.skill_id
-WHERE jpf.job_title_short = 'Data Engineer' AND jpf.job_work_from_home = TRUE
+  ON sjd.skill_id = sd.skill_id
+WHERE jpf.job_title_short = 'Data Engineer' 
+  AND jpf.job_work_from_home = TRUE
 GROUP BY sd.skills
 ORDER BY demand_count DESC
 LIMIT 10;
--- 1_EDA/02_top_paying_skills.sql
+++ 1_EDA/02_top_paying_skills.sql
@@ -10,14 +10,15 @@ Question
 */
 
 SELECT sd.skills,
-ROUND(MEDIAN(jpf.salary_year_avg),0) AS median_salary,
-COUNT(jfp.*) AS demand_count
+  ROUND(MEDIAN(jpf.salary_year_avg),0) AS median_salary,
+  COUNT(jfp.*) AS demand_count
 FROM job_postings_fact AS jpf
 INNER JOIN skills_job_dim AS sjd
-ON jpf.job_id = sjd.job_id
+  ON jpf.job_id = sjd.job_id
 INNER JOIN skills_dim sd
-ON sjd.skill_id = sd.skill_id
-WHERE jpf.job_title_short = 'Data Engineer' AND jpf.job_work_from_home = TRUE
+  ON sjd.skill_id = sd.skill_id
+WHERE jpf.job_title_short = 'Data Engineer' 
+  AND jpf.job_work_from_home = TRUE
 GROUP BY sd.skills
 HAVING COUNT (jfp.*) > 100
 ORDER BY median_salary DESC
